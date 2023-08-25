17808a8389ba665f5bc15b0ec507550daf6616ee
Iryna220 <ibiriszka951@gmail.com>
2023-08-25 20:37:53 +0200

    refaktoring
    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
@@ -6,38 +6,26 @@ import Filter from './Filter';
 import css from './App.module.css';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const scoredContacts = localStorage.getItem('contacts');
-    if (scoredContacts) {
-      this.setState({ contacts: JSON.parse(scoredContacts) });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
 
-  isDublicate(name) {
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const isDublicate = name => {
     const normalizedName = name.toLowerCase();
-    const { contacts } = this.state;
     const result = contacts.find(({ name }) => {
       return name.toLowerCase() === normalizedName;
     });
     return Boolean(result);
-  }
-  addContact = ({ name, number }) => {
-    if (this.isDublicate(name)) {
+  };
+  const addContact = ({ name, number }) => {
+    if (isDublicate(name)) {
       alert(`${name} is already in contacts`);
       return false;
     }
@@ -47,49 +35,44 @@ class App extends Component {
       number: number,
     };
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts(prevContacts => [...prevContacts, contact]);
     return true;
   };
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value.trim());
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLocaleLowerCase();
     return contacts.filter(contact =>
       contact.name.toLocaleLowerCase().includes(normalizedFilter)
     );
   };
-  deleteContact = todoId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
-    }));
+  const deleteContact = todoId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== todoId)
+    );
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <div className={css.phonebookSection}>
-        <h1 className={css.phonebookTitle}>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2 className={css.phonebookContacts}>Contacts</h2>
-        <div className={css.allContacts}>All contacts: {contacts.length}</div>
-        {contacts.length > 0 ? (
-          <>
-            <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList
-              contacts={this.getVisibleContacts()}
-              onDeleteContact={this.deleteContact}
-            />
-          </>
-        ) : (
-          <Notification message="Contact list is empty" />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className={css.phonebookSection}>
+      <h1 className={css.phonebookTitle}>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2 className={css.phonebookContacts}>Contacts</h2>
+      <div className={css.allContacts}>All contacts: {contacts.length}</div>
+      {contacts.length > 0 ? (
+        <div>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={getVisibleContacts()}
+            onDeleteContact={deleteContact}
+          />
+        </div>
+      ) : (
+        <Notification message="Contact list is empty" />
+      )}
+    </div>
+  );
 }
+
 export default App;
